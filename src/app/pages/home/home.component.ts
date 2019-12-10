005f4c9662f97be26f5aabd35b682eccf826be82
Vitaly <vitalykuklin@gmail.com>
2019-12-10 07:26:22 +0700

    Feature: add dinamic teg properties [ref: feature add changeColor function]

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public title = 'myApp';
+  public color = 'red';
+  public myClass = 'colorBlue';
+  public textSize = 150;
   constructor() { }
+  changeColor(color) {
 
+  }
   ngOnInit() {
   }
 
