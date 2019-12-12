92083dfa0fd55320130abed8ab939e3f3f75a1ea
Vitaly <vitalykuklin@gmail.com>
2019-12-13 06:48:26 +0700

    Feature: continue screencast - ngIf - ngFor [ref:no-ref]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  constructor() { }
+export class AppComponent implements OnInit {
+  public user = {
+    name: 'Vitaly'
+  };
+  public isShown = true;
+
+  toggle() {
+    this.isShown = !this.isShown;
+  }
+
+  constructor() {
+
+  }
+
   ngOnInit() {
   }
 }
