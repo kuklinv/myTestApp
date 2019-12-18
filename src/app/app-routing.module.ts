776bb96325885183504731a1b360e07c4819c539
Vitaly <vitalykuklin@gmail.com>
2019-12-19 06:53:06 +0700

    Feature: add router navi for main menu [ref: add navigation]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {SkillsComponent} from './pages/skills/skills.component';
+import {PortfolioComponent} from './pages/portfolio/portfolio.component';
+import {ProjectsComponent} from './pages/projects/projects.component';
+import {BlogComponent} from './pages/blog/blog.component';
+import {SandboxComponent} from './pages/sandbox/sandbox.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'projects' , component: ProjectsComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'sandbox', component: SandboxComponent},
+  { path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
