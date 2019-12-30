96d78c217dc05e40d05ec4758de6100fda3aa969
Vitaly <vitalykuklin@gmail.com>
2019-12-30 14:37:34 +0700

    Feature:add media-list to learn page[ref:change to my custom sertificats]

--- src/app/pages/learn/carousel/carousel.component.ts
+++ src/app/pages/learn/carousel/carousel.component.ts
@@ -1,23 +1,13 @@
-import {Component, ViewChild, ViewEncapsulation} from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
-} from '@angular/animations';
+import {Component} from '@angular/core';
+
 
 @Component({
     selector: 'app-ngbd-carousel',
     templateUrl: './carousel.component.html',
-    encapsulation: ViewEncapsulation.None,
     styleUrls: ['./carousel.component.css']
   }
 )
 
-  export class NgbdCarouselBasic {
-  images = ["https://picsum.photos/900/500?random&t=1",
-    "https://picsum.photos/900/500?random&t=2",
-    "https://picsum.photos/900/500?random&t=3"]
+export class NgbdCarouselBasic {
 
 }
